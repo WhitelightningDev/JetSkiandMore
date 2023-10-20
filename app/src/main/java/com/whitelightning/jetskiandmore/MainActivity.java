d530095d763d2ea9a6b8bef20a72e1606c68acd4
WhitelightningDev <danielmommsen@hotmail.com>
2023-10-20 17:46:59 +0200

    Added Pull down refresh functionality as well as the dependencies added The application logo Changed the activity_main.xml layout to add the refresh id

--- app/src/main/java/com/whitelightning/jetskiandmore/MainActivity.java
+++ app/src/main/java/com/whitelightning/jetskiandmore/MainActivity.java
@@ -3,14 +3,20 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 public class MainActivity extends Activity {
     private WebView webView;
+    private SwipeRefreshLayout swipeRefreshLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        swipeRefreshLayout = findViewById(R.id.swipeRefreshLayout);
+        webView = findViewById(R.id.webView);
+
 
         webView = findViewById(R.id.webView);
 
@@ -22,7 +28,44 @@ public class MainActivity extends Activity {
 
         // Load a URL in the WebView
         webView.loadUrl("https://www.jetskiandmore.com");
+
+        // Handle back button navigation in the WebView
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+
+                // Inject custom CSS to remove padding
+                String customCss = "body { margin: 0; padding: 0; }";
+                view.loadUrl("javascript:(function() {" +
+                        "var style = document.createElement('style');" +
+                        "style.innerHTML = '" + customCss + "';" +
+                        "document.head.appendChild(style);" +
+                        "})()");
+            }
+        });
+
+
+
+        // Handle pull-down to refresh
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                webView.reload(); // Refresh the WebView when pulled down
+            }
+        });
+
+        // Handle the end of the refresh operation
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                swipeRefreshLayout.setRefreshing(false); // Hide the refresh indicator
+            }
+        });
     }
+
+
 
     // Handle back button navigation in the WebView
     @Override
